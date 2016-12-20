918f3d06a70c47c583201a18fcda4f072f212370
Weili Yi <yi@weiliy.net>
2016-12-20 09:49:21 +0800

    feat: init module and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ido',
+      version='draft',
+      description='I Do Task',
+      author='Weili Yi',
+      author_email='yi@weiliy.net',
+      packages=['ido'],
+     )
